b3da1862b3dbce76a8af47ec2e2876df3ef91745
hibana2077 <hibana2077@gmail.com>
2025-10-17 21:28:51 +0800

    fix: Update virtual environment and cache paths in T002.sh script

--- reference/T002.sh
+++ reference/T002.sh
@@ -9,8 +9,8 @@
 #PBS -l storage=scratch/rp06
 
 module load cuda/12.6.2
-source /scratch/rp06/xx12345/AMBER/.venv/bin/activate
-export HF_HOME="/scratch/rp06/xx12345/AMBER/.cache"
+source /scratch/rp06/xx12345/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/xx12345/TC-BombKD/.cache"
 
 cd ../..
 python3 -m src.run train --data-root "./datasets/ucf101_subset/temp_download/extracted/UCF101_subset" --output-dir "./runs/videomae-ucf" --epochs 100 --batch-size 2 --no_tqdm >> T002.log 2>&1
