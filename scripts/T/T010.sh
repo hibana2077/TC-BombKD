51ea2f28a4572a554cbb4b2f3021b404a32b709c
hibana2077 <hibana2077@gmail.com>
2025-10-23 10:30:28 +0800

    feat: Add initial script for training fusion model with specified parameters

---
+++ scripts/T/T010.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=24GB
+#PBS -l walltime=02:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+# python3 -m polyspace.data.featurize --dataset hmdb51 --root ./datasets/hmdb51 --split train --out ./features --student vjepa2 --teachers videomae timesformer vivit --batch 2 --workers 2 --frames 16 >> T002.log 2>&1
+# python3 -m polyspace.train.inspect_features --features ./features/features_hmdb51_train.index.json --limit 100 --sample 3 >> T002.log 2>&1
+python3 -m polyspace.train.train_fusion \
+    --dataset hmdb51 \
+    --root ./datasets/hmdb51 \
+    --split train \
+    --student vjepa2 \
+    --teachers vivit \
+    --converters ./checkpoints/converters_d_9.pt \
+    --classes 51 \
+    --frames 16 \
+    --batch 8 \
+    --epochs 10 \
+    --lr 3e-4 \
+    --save_dir ./checkpoints/fusion_final
