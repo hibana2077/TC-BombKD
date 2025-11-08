427ec57243157458a0c0d565744807f6b53fd117
hibana2077 <hibana2077@gmail.com>
2025-11-09 09:13:49 +1000

    fix: Increase marker size in visualization script for better clarity

--- scripts/VIS/VU002.sh
+++ scripts/VIS/VU002.sh
@@ -22,7 +22,7 @@ python3 -m polyspace.vis.vis_ct_orig \
   --student vjepa2 \
   --teachers videomae timesformer vivit \
   --all_teachers \
-  --marker_size 100 \
+  --marker_size 160 \
   --converters ./checkpoints/U010/converters_ep10.pt \
   --frames 16 \
   --per_class 50 \
