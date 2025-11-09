e30d7037b79b9faf64704520ceec37126fd308ec
hibana2077 <hibana2077@gmail.com>
2025-11-10 09:16:55 +1000

    feat: Add ABL scripts for AR model training with various features and remap option

---
+++ scripts/ABL/ABLS004.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=32GB
+#PBS -l walltime=04:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -m polyspace.train.feature_cls \
+  --task ar \
+  --train ./features/ssv2/features_ssv2_train.index.json \
+  --test ./features/ssv2/features_ssv2_test.index.json \
+  --feature videomae \
+  --remap_by_order \
+  --ar_models svm lr >> ABLS003.log 2>&1
