df5ce0161f14604d675214c38f44322b394f5b40
hibana2077 <hibana2077@gmail.com>
2025-11-10 20:43:32 +1000

    feat: Add S004.sh and S005.sh for training converters and fusion, and S006.sh for downstream evaluation with new dataset paths and configurations

---
+++ scripts/S/S004.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=24GB
+#PBS -l walltime=48:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+# python3 -m polyspace.data.featurize --dataset hmdb51 --root ./datasets/hmdb51 --split train --out ./features --student vjepa2 --teachers videomae timesformer vivit --batch 2 --workers 2 --frames 16 >> T002.log 2>&1
+# python3 -m polyspace.train.inspect_features --features ./features/features_hmdb51_train.index.json --limit 100 --sample 3 >> T002.log 2>&1
+python3 -m polyspace.train.train_converter \
+	--features ./features/ssv2/features_ssv2_train.index.json \
+	--teachers videomae timesformer vivit \
+	--d_in 1024 --d_out 768 \
+	--kind b \
+	--epochs 10 \
+	--batch 32 \
+	--workers 1 \
+    --log_every 1000 \
+    --pin_memory \
+	--loss_l2 0.0 \
+	--loss_cos 0.0 \
+	--loss_nce 0.0 \
+	--loss_vic 1.0 \
+	--loss_bar 0.0 \
+	--loss_l1 0.0 \
+	--save_dir ./checkpoints/S004 >> S004.log 2>&1
---
+++ scripts/S/S005.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q dgxa100
+#PBS -l ngpus=1
+#PBS -l ncpus=16
+#PBS -l mem=24GB
+#PBS -l walltime=48:00:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+# python3 -m polyspace.data.featurize --dataset hmdb51 --root ./datasets/hmdb51 --split train --out ./features --student vjepa2 --teachers videomae timesformer vivit --batch 2 --workers 2 --frames 16 >> T002.log 2>&1
+# python3 -m polyspace.train.inspect_features --features ./features/features_hmdb51_train.index.json --limit 100 --sample 3 >> T002.log 2>&1
+python3 -m polyspace.train.train_fusion \
+    --dataset ssv2 \
+    --root ./features/ssv2/features_ssv2_train.index.json \
+    --split train \
+    --student vjepa2 \
+    --teachers videomae timesformer vivit \
+    --converters ./checkpoints/S004/converters_ep10.pt \
+    --classes 174 \
+    --frames 16 \
+    --batch 8 \
+    --epochs 20 \
+    --lr 3e-4 \
+    --use_cached_features \
+    --features_fp16 \
+    --save_dir ./checkpoints/S005 >> S005.log 2>&1
