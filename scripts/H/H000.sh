6c50884a3558e72a12d1c63b740c65c9f0594233
hibana2077 <hibana2077@gmail.com>
2025-11-16 14:29:45 +1000

    del: Remove old exp and add new setup

--- scripts/H/H000.sh
+++ scripts/H/H000.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -P rp06
-#PBS -q gpuvolta
+#PBS -q dgxa100
 #PBS -l ngpus=1
-#PBS -l ncpus=12
-#PBS -l mem=24GB
-#PBS -l walltime=12:00:00
+#PBS -l ncpus=16
+#PBS -l mem=64GB
+#PBS -l walltime=48:00:00
 #PBS -l wd
 #PBS -l storage=scratch/rp06
 
@@ -18,16 +18,18 @@ cd ../..
 # 備註：若不加 --shard_size 仍可輸出單一 pkl；為避免記憶體爆炸，建議啟用分片與 fp16。
 # 產出：features_ucf101_train.index.json + 多個 features_ucf101_train_shard_XXXXX.pkl
 python3 -m polyspace.data.featurize \
-	--dataset hmdb51 \
-	--root ./datasets/hmdb51 \
-	--split train \
-	--out ./features \
-	--student vjepa2 \
-	--teachers videomae timesformer vivit \
-	--batch 2 \
-	--workers 2 \
-	--frames 16 \
-	--shard_size 512 \
-	--fp16 \
-	--no_tqdm \
-	>> H000.log 2>&1
+  --dataset hmdb51 \
+  --root ./datasets/hmdb51 \
+  --split train \
+  --out ./features/hmdb51 \
+  --student vjepa2ssv2 \
+  --teachers vivit videomaeg timesformerg \
+  --batch 2 \
+  --workers 2 \
+  --student_frames 64 \
+  --teacher_frames 32 16 16 \
+  --shard_size 512 \
+  --storage npy_dir \
+  --fp16 \
+  --no_tqdm \
+  >> H000.log 2>&1
