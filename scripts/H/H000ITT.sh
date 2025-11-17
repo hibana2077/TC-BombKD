41421ff3b09fd676f98c1dfb994e9be802634647
hibana2077 <hibana2077@gmail.com>
2025-11-17 11:50:54 +1000

    fix: Add missing --report-anomaly flag in inspect_features command for H000ITT.sh

--- scripts/H/H000ITT.sh
+++ scripts/H/H000ITT.sh
@@ -3,7 +3,7 @@
 #PBS -q gpuvolta
 #PBS -l ngpus=1
 #PBS -l ncpus=12
-#PBS -l mem=24GB
+#PBS -l mem=64GB
 #PBS -l walltime=12:00:00
 #PBS -l wd
 #PBS -l storage=scratch/rp06
@@ -17,5 +17,5 @@ export HF_HUB_OFFLINE=1
 cd ../..
 # 備註：若不加 --shard_size 仍可輸出單一 pkl；為避免記憶體爆炸，建議啟用分片與 fp16。
 # 產出：features_hmdb51_train.index.json + 多個 features_hmdb51_train_shard_XXXXX.pkl
-python3 -m polyspace.train.inspect_features --features ./features/hmdb51/features_hmdb51_test.index.json --limit 100 --sample 3 >> H000ITT.log 2>&1
+python3 -m polyspace.train.inspect_features --features ./features/hmdb51/features_hmdb51_test.index.json --limit 100 --sample 3 --report-anormaly >> H000ITT.log 2>&1
 python3 -m polyspace.train.scan_labels --index ./features/hmdb51/features_hmdb51_test.index.json --report >> H000ITT.log 2>&1
