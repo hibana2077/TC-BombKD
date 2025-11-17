51ae2040f5b59ff3c08d204218a6cba8c7a45946
hibana2077 <hibana2077@gmail.com>
2025-11-17 11:45:28 +1000

    feat: Add new training scripts H002 to H009 with configurations for converter and fusion models

---
+++ scripts/H/H005.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=50GB
+#PBS -l walltime=40:30:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -m polyspace.train.train_converter \
+	--features ./features/hmdb51/features_hmdb51_train.index.json \
+	--teachers vivit videomaeg timesformerg \
+	--d_in 1408 --d_out 768 \
+	--kind b \
+	--epochs 10 \
+	--batch 16 \
+	--workers 1 \
+	--log_every 20 \
+	--pin_memory \
+	--loss_l2 0.1 \
+	--loss_cos 0.1 \
+	--loss_nce 0.2 \
+	--loss_vic 0.05 \
+	--loss_bar 0.55 \
+	--loss_l1 0.0 \
+	--save_dir ./checkpoints/H005/converter >> H005.log 2>&1
+
+python3 -m polyspace.train.train_fusion \
+    --features ./features/hmdb51/features_hmdb51_train.index.json \
+    --teachers vivit videomaeg timesformerg \
+    --converters ./checkpoints/H005/converters_ep10.pt \
+    --classes 51 \
+    --batch 8 \
+    --epochs 50 \
+    --lr 3e-4 \
+    --features_fp16 \
+    --save_dir ./checkpoints/H005/fusion >> H005.log 2>&1
+
+for ep in {1..50}; do
+  echo "checkpoint: ep$ep" >> H005.log 2>&1
+  python3 -m polyspace.train.eval_downstream \
+    --features ./features/features_hmdb51_test.index.json \
+    --teachers vivit videomaeg timesformerg \
+    --converters ./checkpoints/H005/converter/converters_ep10.pt \
+    --fusion ./checkpoints/H005/fusion/fusion_ep$ep.pt \
+    --features_fp16 >> H005E.log 2>&1
+done
