09f3eb6c574bf0c89e5e590adba95e2f56978c9f
hibana2077 <hibana2077@gmail.com>
2025-11-18 11:56:53 +1000

    feat: Add new training scripts HS002 to HS012 with updated configurations and loss parameters

---
+++ scripts/HS/HS008.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#PBS -P kf09
+#PBS -q gpuvolta
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=50GB
+#PBS -l walltime=40:30:00
+#PBS -l wd
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+module load ffmpeg/4.1.3
+source /scratch/rp06/sl5952/TC-BombKD/.venv/bin/activate
+export HF_HOME="/scratch/rp06/sl5952/TC-BombKD/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -m polyspace.train.train_converter \
+	--features ./features/hmdb51/features_hmdb51_train.index.json \
+	--teachers vivit videomaeg timesformerg \
+	--d_in 1408 --d_out 768 \
+	--d_out_map "vivit=768,videomaeg=1280,timesformerg=768" \
+	--kind c \
+	--epochs 10 \
+	--batch 4 \
+	--workers 1 \
+	--log_every 20 \
+	--pin_memory \
+	--loss_l2 0.0 \
+	--loss_cos 1.0 \
+	--loss_nce 0.0 \
+	--loss_vic 0.0 \
+	--loss_bar 0.0 \
+	--loss_l1 0.0 \
+    --subsample_ratio 0.1 \
+	--save_dir ./checkpoints/HS008/converter >> HS008.log 2>&1
+
+python3 -m polyspace.train.train_fusion \
+	--features ./features/hmdb51/features_hmdb51_train.index.json \
+	--teachers vivit videomaeg timesformerg \
+	--converters ./checkpoints/HS008/converter/converters_ep10.pt \
+	--classes 51 \
+	--batch 4 \
+	--epochs 50 \
+	--lr 3e-4 \
+	--features_fp16 \
+    --subsample_ratio 0.1 \
+	--save_dir ./checkpoints/HS008/fusion >> HS008.log 2>&1
+
+for ep in {1..50}; do
+  echo "checkpoint: ep$ep" >> HS008.log 2>&1
+	python3 -m polyspace.train.eval_downstream \
+		--features ./features/hmdb51/features_hmdb51_test.index.json \
+		--teachers vivit videomaeg timesformerg \
+		--converters ./checkpoints/HS008/converter/converters_ep10.pt \
+		--fusion ./checkpoints/HS008/fusion/fusion_ep$ep.pt \
+		--features_fp16 >> HS008E.log 2>&1
+done
